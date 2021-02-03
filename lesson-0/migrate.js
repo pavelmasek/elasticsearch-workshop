a07a49fcbfdfb572c0fab427cd4e6eef74c9bf50
Pavel Masek <pavel.masek@socialbakers.com>
2021-02-03 13:23:29 +0100

    Added mapping

--- lesson-0/migrate.js
+++ lesson-0/migrate.js
@@ -1,5 +1,6 @@
 const { Client } = require('elasticsearch')
 const dataset = require('./elasticsearch-workshop-lesson-0.json')
+const mapping = require('./lesson-0-mapping.json')
 
 const client = new Client({
   node: 'http://localhost:9200'
@@ -10,6 +11,7 @@ const indexName = 'posts-lesson-0'
 async function run () {
     const indexCreatinResponse = await client.indices.create({
       index: indexName,
+      body: mapping,
     }, { ignore: [400] })
 
     const body = dataset.flatMap(doc => [{ index: { _index: indexName } }, doc])
